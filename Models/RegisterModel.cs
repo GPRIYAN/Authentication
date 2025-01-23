0b7c8cf10e38ede5ae00b49b7e6e0567e1d9955d
Priyan G <120967352+GPRIYAN@users.noreply.github.com>
2025-01-23 19:05:34 +0530

    Update RegisterModel.cs

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -7,9 +7,6 @@ namespace WiseMaestroRBAC.Models
         [Required(ErrorMessage = "Username is required.")]
         public string? Username { get; set; }
 
-        [Required(ErrorMessage = "Role is required.")]
-        public string? Role { get; set; }
-
         [Required(ErrorMessage = "Email is required.")]
         public string? Email { get; set; }
 
